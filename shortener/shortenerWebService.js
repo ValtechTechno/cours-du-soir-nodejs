fbf0ca8622c339e78458fb861f9a04f9f7f3ddfc
Grégory <paulgreg@thinkgreg.(none)>
2011-06-09 13:48:42 +0200

    Fixes demo code and temporary add express lib

--- shortener/shortenerWebService.js
+++ shortener/shortenerWebService.js
@@ -1,40 +1,21 @@
 var sys = require("sys"),  
     http = require("http"),
     md5 = require("./jshash-2.2/md5_export"),
-    shortener = require("./shortener");
+    shortener = require("./shortener"),
+    express = require('express');
 
+var port = 8000;
 
+var app = express.createServer();
+app.configure(function() {
+	app.use(app.router);
+	app.use(express.static(__dirname));
+}).listen(port);
+console.log("listening on " + port);
 
-http.createServer(function(request, response) {  
-
-	var url = request.url;
-	var param = require('url').parse(url).query;
-	console.log("param: " + param);
-	if (param){
-		var index = param.indexOf('=');
-		var urlToShorten = param.substr(index + 1, param.length);
-		console.log("urlToShorten: " + urlToShorten);
-		var shortened = shortener.shorten(urlToShorten);
-		console.log("url shorten: " + shortened);
-
-		response.writeHead(200, {"Content-Type": "text/plain"});  
-	    	//response.write(shortened);
-response.write("{url: "+ shortened +"}");    
-	    	response.end();
-
-	}else{
-		console.log("no url to shorten !");
-		response.writeHead(200, {"Content-Type": "text/plain"});  
-	    	response.write("Hello " + "shortener");  
-	    	response.end("Hello " + "shortener");
-
-	}
-
-
-	
-
-    	
-}).listen(8080);  
-  
-
-
+app.get('/shorten/:url', function(req, res){
+	var urlToShorten = req.params.url;
+	console.log("urlToShorten: " + urlToShorten);
+	var shortened = shortener.shorten(urlToShorten);
+   res.send(shortened);
+});
